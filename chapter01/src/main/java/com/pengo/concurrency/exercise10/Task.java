a8a34cfce07c34682be0d9d1fcd34a5fa545c688
Benpeng Wang <961888137@qq.com>
2020-06-20 23:29:03 +0800

    完成线程工厂创建线程 完成第一章

---
+++ chapter01/src/main/java/com/pengo/concurrency/exercise10/Task.java
@@ -0,0 +1,20 @@
+package com.pengo.concurrency.exercise10;
+
+import java.util.concurrent.TimeUnit;
+
+public class Task implements Runnable
+{
+  @Override
+  public void run()
+  {
+    try
+    {
+      TimeUnit.SECONDS.sleep(1);
+    }
+    catch(InterruptedException e)
+    {
+      e.printStackTrace();
+    }
+
+  }
+}
