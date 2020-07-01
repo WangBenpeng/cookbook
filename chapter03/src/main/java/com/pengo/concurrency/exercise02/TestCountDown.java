c975a3a70eb939f6146bb52b484fe773351dd04e
Benpeng Wang <961888137@qq.com>
2020-07-01 22:16:08 +0800

    等待多个并发事件

---
+++ chapter03/src/main/java/com/pengo/concurrency/exercise02/TestCountDown.java
@@ -0,0 +1,17 @@
+package com.pengo.concurrency.exercise02;
+
+public class TestCountDown
+{
+  public static void main(String[] args)
+  {
+    VideoConference conference = new VideoConference(10);
+    Thread threadConference = new Thread(conference);
+    threadConference.start();
+    for(int i = 0; i < 10; i++)
+    {
+      Participant participant = new Participant(conference, "Participant " + i);
+      Thread thread = new Thread(participant);
+      thread.start();
+    }
+  }
+}
---
+++ chapter03/src/main/java/com/pengo/concurrency/exercise02/VideoConference.java
@@ -0,0 +1,40 @@
+package com.pengo.concurrency.exercise02;
+
+import java.util.concurrent.CountDownLatch;
+
+public class VideoConference implements Runnable
+{
+  private final CountDownLatch controller;
+
+  public VideoConference(int number)
+  {
+    controller = new CountDownLatch(number);
+  }
+
+  /**
+   * 参会人员到达
+   * @param name
+   */
+  public void arrive(String name)
+  {
+    System.out.printf("%s has arrived\n", name);
+    controller.countDown();
+    System.out.printf("VideoConference: Waiting for %d participants.\n", controller.getCount());
+  }
+
+  @Override
+  public void run()
+  {
+    System.out.printf("VideoConference: Initialization: %d participants.\n", controller.getCount());
+    try
+    {
+      controller.await();
+      System.out.printf("VideoConference: All the participants have come\n");
+      System.out.printf("VideoConference: Let's start...\n");
+    }
+    catch(InterruptedException e)
+    {
+      e.printStackTrace();
+    }
+  }
+}
