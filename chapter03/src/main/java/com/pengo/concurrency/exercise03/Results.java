463af37c2e76735c5ff011d9d15a0e05ac47a486
Benpeng Wang <961888137@qq.com>
2020-07-06 23:42:56 +0800

    等待多个并发事件

---
+++ chapter03/src/main/java/com/pengo/concurrency/exercise03/Results.java
@@ -0,0 +1,21 @@
+package com.pengo.concurrency.exercise03;
+
+public class Results
+{
+  private final int data[];
+
+  public Results(int size)
+  {
+    data = new int[size];
+  }
+
+  public void setData(int position, int value)
+  {
+    data[position] = value;
+  }
+
+  public int[] getData()
+  {
+    return data;
+  }
+}
