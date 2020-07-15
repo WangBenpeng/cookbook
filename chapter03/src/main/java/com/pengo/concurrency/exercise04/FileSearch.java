b54855e116a7709c0278610df00bb89d0faf495e
WangBenpeng <961888137@qq.com>
2020-07-16 00:00:57 +0800

    阶段性并发任务

---
+++ chapter03/src/main/java/com/pengo/concurrency/exercise04/FileSearch.java
@@ -0,0 +1,63 @@
+package com.pengo.concurrency.exercise04;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Phaser;
+import java.util.concurrent.TimeUnit;
+
+public class FileSearch {
+    private final String initPath;
+    private final String fileExtension;
+    private List<String> results;
+    private Phaser phaser;
+
+    public FileSearch(String initPath, String fileExtension, Phaser phaser) {
+        this.initPath = initPath;
+        this.fileExtension = fileExtension;
+        this.phaser = phaser;
+        results = new ArrayList<>();
+    }
+
+    /**
+     * 处理file文件及其子文件夹
+     * @param file
+     */
+    private void directoryProcess(File file) {
+        File list[] = file.listFiles();
+        if (list != null) {
+            for (int i = 0; i < list.length; i++) {
+                if (list[i].isDirectory()) {
+                    directoryProcess(list[i]);
+                } else {
+                    fileProcess(list[i]);
+                }
+            }
+        }
+    }
+
+    /**
+     * 是否与指定的扩展名相同
+     * @param file
+     */
+    private void fileProcess(File file) {
+        if (file.getName().endsWith(fileExtension)) {
+            results.add(file.getAbsolutePath());
+        }
+    }
+
+    private void filterResults() {
+        List<String> newResults = new ArrayList<>();
+        long actualDate = new Date().getTime();
+        for (int i = 0; i < results.size(); i++) {
+            File file = new File(results.get(i));
+            long fileDate = file.lastModified();
+            if (actualDate - fileDate < TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS)) {
+                newResults.add(results.get(i));
+            }
+            results = newResults;
+        }
+    }
+
+}
